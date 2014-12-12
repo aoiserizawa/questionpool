77bc264e5958b397819d125038bdd20f50be6afa
ijtrinidad <ijthreedee@yahoo.com>
2014-12-12 15:59:50 +0800

    fixed pagination arrow.

--- app/views/sessions/questionlist.blade.php
+++ app/views/sessions/questionlist.blade.php
@@ -30,7 +30,7 @@
                 <li><a href="">2</a></li>
                 <li><a href="">3</a></li>
                 <li><a href="">4</a></li>
-                <li><a href="#"><span aria-hidden="true">&laquo;</span><span class="sr-only">Next</span></a></li>
+                <li><a href="#"><span aria-hidden="true">&raquo;</span><span class="sr-only">Next</span></a></li>
             </ul>
         </nav>
 @stop
