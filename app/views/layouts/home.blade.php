8ac55df4859560c6988b94896bd419d9fc25db76
rkm143jpat <ronkevinmanuela@gmail.com>
2015-01-08 12:10:43 +0800

    Fixed home

--- app/views/layouts/home.blade.php
+++ app/views/layouts/home.blade.php
@@ -31,7 +31,7 @@
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
                     <li><a href="settings">Settings</a></li>
-                    {{HTML::linkAction('SessionsController@destroy','Logout')}}
+                    <li>{{HTML::linkAction('SessionsController@destroy','Logout')}}</li>
                     <!-- <li class="dropdown">
                       <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
                       <ul class="dropdown-menu" role="menu">
